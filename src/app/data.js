612119f85164316cea0795a511e7f60a182684b4
SAMIR <109948887+mdsamir8863@users.noreply.github.com>
2023-06-06 18:45:44 +0530

    first commit

---
+++ src/app/data.js
@@ -0,0 +1,64 @@
+export const quiz = {
+  totalQuestions: 8,
+  questions: [
+    {
+      id: 1,
+      question: "What is the capital of France?",
+      answers: ["Madrid", "Paris", "Rome", "Berlin"],
+      correctAnswer: "Paris",
+    },
+    {
+      id: 2,
+      question: "What is the largest planet in our solar system?",
+      answers: ["Mars", "Jupiter", "Venus", "Saturn"],
+      correctAnswer: "Jupiter",
+    },
+    {
+      id: 3,
+      question: "What is the smallest country in the world?",
+      answers: ["Monaco", "Maldives", "Vatican City", "San Marino"],
+      correctAnswer: "Vatican City",
+    },
+    {
+      id: 4,
+      question: "What is the most widely spoken language in the world?",
+      answers: ["English", "Mandarin", "Spanish", "Hindi"],
+      correctAnswer: "Mandarin",
+    },
+    {
+      id: 5,
+      question: "Who is the founder of Microsoft?",
+      answers: ["Steve Jobs", "Bill Gates", "Elon Musk", "Mark Zuckerberg"],
+      correctAnswer: "Bill Gates",
+    },
+    {
+      id: 6,
+      question: "Who is the creator of this quiz app ?",
+      answers: [
+        "Steve Jobs",
+        "Bill Gates",
+        "Md Samir Ansari",
+        "Mark Zuckerberg",
+      ],
+      correctAnswer: "Md Samir Ansari",
+    },
+    {
+      id: "7",
+      question: "Which is the best lord?",
+      answers: [
+        "Lord of the Rings",
+        "Lord of the Flies",
+        "Lord of the Dance",
+        "Lorde",
+      ],
+      correctAnswer: "Lord of the Rings",
+    },
+
+    {
+      id: "8",
+      question: "Which computer languages have you used?",
+      answers: ["JavaScript", "Java", "C", "Python", "Ook", "LISP"],
+      correctAnswer: "JavaScript",
+    },
+  ],
+};
